db10fcb6f0e9f6a921768d86b87385984a6f9955
sarah <messy.earth@gmail.com>
2023-09-07 11:35:35 -0700

    prog

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
-int main(void){
+/**
+ * main-entry
+ * A C programming that print function
+ * return always 0
+ */
+int main(void)
+{
 puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
