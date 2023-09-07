b9cc32a9971fb807f309e6b62dfedaec7b293fdb
sarah <messy.earth@gmail.com>
2023-09-07 15:25:21 -0700

    prog

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,10 @@ include < stdio.h >
  */
 int main(void)
 {
-printf("Size of a char:%lu byte(s)\n", Sizeof(char));
-printf("Size of an int :%lu byte(s)\n", Sizeof(int));
-printf("Size of a long int:%lu byte(s)\n", Sizeof(long int));
-printf("Size of a long long int:%lu byte(s)\n", Sizeof(long long int));
-printf("Size of a float:%lu byte(s)\n", Sizeof(float));
+printf("Size of a char:%u byte(s)\n", Sizeof(char));
+printf("Size of an int :%u byte(s)\n", Sizeof(int));
+printf("Size of a long int:%u byte(s)\n", Sizeof(long int));
+printf("Size of a long long int:%u byte(s)\n", Sizeof(long long int));
+printf("Size of a float:%u byte(s)\n", Sizeof(float));
 return (0);
 }
