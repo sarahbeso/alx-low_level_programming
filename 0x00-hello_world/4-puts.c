3ba92b0cb3ebf9df44c3a4387523943ae4a5a7f1
sarah <messy.earth@gmail.com>
2023-09-07 11:22:52 -0700

    prog

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
 int main (void){
-put ("Programming is like building a multilingual puzzle")
-	return(0)
+put ("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
