2a61e3ea914974dcc1356753d5fac83bd0d89507
sarah <messy.earth@gmail.com>
2023-09-08 09:24:16 -0700

    func

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - Entry point
+ * Description: print all alphabet letter
+ * Return: 0 (success)
+ */
+int main(void)
+{
+int digit = 0;
+if (digit <= 9)
+{
+printf("%i", digit);
+}
+else
+{
+digit++
+}
+}
