404e374886ce3cda57f79eada8f51835b3d06ce4
sarah <messy.earth@gmail.com>
2023-09-07 18:12:17 -0700

    prog

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,11 +5,11 @@
  * main - Entry point
  * Description: print a qoute using write functio
  * write (int fd ,const void *buf,size_t count)
- * Return: 1(not succecc)
+ * Return: 1 (not succecc)
  */
 int main(void)
 {
 char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 write(1, quo, 59);
 return (1);
-
+}
