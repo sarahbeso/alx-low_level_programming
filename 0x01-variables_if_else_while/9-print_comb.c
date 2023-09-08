c5ff1b1b75f68205ee773d0e24e3b51931d426b4
sarah <messy.earth@gmail.com>
2023-09-08 13:31:47 -0700

    func

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Description: print numbers of base in lowercase
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+int numbers = '0';
+{
+while (numbers <= '9')
+{
+putchar(numbers);
+if (numbers != 9)
+{
+putchar(',');
+}
+putchar('\n');
+return (0);
+}
