823aaba053609343a70118979576d3c3e365a19b
sarah <messy.earth@gmail.com>
2023-09-08 09:06:37 -0700

    func

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+* main - Entry point
+ * Description: print all alphabet letter
+ * Return: 0 (success)
+ */
+int main(void)
+{
+char ch = 'a';
+while (ch < 'z')
+{
+if (ch == 'e' || ch == 'q')
+ch++;
+putchar(ch);
+ch++;
+}
+putchar('\n')
+return (0);
+}
