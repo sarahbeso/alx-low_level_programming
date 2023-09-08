5c2c3a88202e7017d4675ef254180558c81b8252
sarah <messy.earth@gmail.com>
2023-09-08 08:17:55 -0700

    func

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+* main - Entry point
+ * Description: print a value of n if it is greater
+ * than 5 or equal 0 or less than 6 and not zero.
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char ch = 'a';
+while (ch = < 'z')
+
+{ 
+putchar(ch);
+ch++;
+}
+	putchar('\n');
+	return (0);
+
+}
