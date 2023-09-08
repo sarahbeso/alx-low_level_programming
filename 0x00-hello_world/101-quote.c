6ae0d1452f171ff3f2dbbfba0013b0e298c431f2
sarah <messy.earth@gmail.com>
2023-09-07 18:08:07 -0700

    prog

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,9 @@
 #include <unistd.h>
+
 /**
  * main - Entry point
- * Description: print a qoute using write function
+ * Description: print a qoute using write functio
+ * write (int fd ,const void *buf,size_t count)
  * Return: 1(not succecc)
  */
 int main(void)
@@ -9,3 +11,4 @@ int main(void)
 char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 write(1, quo, 59);
 return (1);
+
