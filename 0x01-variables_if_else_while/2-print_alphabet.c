4c1dd69b1c6e701898f258d8b147277a84336695
sarahbeso <141324606+sarahbeso@users.noreply.github.com>
2023-09-11 13:54:03 +0300

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,6 +5,20 @@
 * main - Entry point
  * Description: print all alphabet letter
  * Return: 0 (success)
+ #include <stdio.h>
+
+int main(void)
+{
+char ch = 'a';
+ for(ch='a'; ch <= 'z'; ch++)
+
+{
+    putchar(ch);
+
+}
+	return (0);
+}
+
  */
 int main(void)
 {
