421ef22c3525800d71b3c58c76936fbfce85c256
sarah <messy.earth@gmail.com>
2023-09-12 15:45:36 -0700

    a

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _abs - check the code
+ *@n: takes in integer type input for function
+ * Return: Always 0.
+ */
+int _abs(int n)
+{
+if (n < 0)
+n = (-1) * n;
+_putchar('n');
+return (0);
+}
