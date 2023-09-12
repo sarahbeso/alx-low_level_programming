f61afaf4ce3b0782f2ecc535316e2812823be97b
sarah <messy.earth@gmail.com>
2023-09-12 13:26:57 -0700

    a

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,7 +8,8 @@
 void print_alphabet_x10(void);
 {
 int i, alpha;
-for (i = 1; i<= 10; i++){
+for (i = 1; i <= 10; i++)
+{
 for (alpha = 'a'; alpha <= 'z'; alpha++)
 _putchar(alpha);
 _putchar('\n');
