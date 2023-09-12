c71f81cccbf5cc0520670174cd42df6646e81b98
sarah <messy.earth@gmail.com>
2023-09-12 06:54:39 -0700

    a

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,8 +6,14 @@
 * Return: Always 0 (success)
 */
 int main(void)
-{ 
-	string a = 'putchar';
-	putchar('a');
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 return (0);
 }
