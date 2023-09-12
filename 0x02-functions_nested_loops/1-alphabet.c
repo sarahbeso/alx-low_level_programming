e4c51c5ac558761c6e0e91732ac70be9a51d03ae
sarah <messy.earth@gmail.com>
2023-09-12 11:56:18 -0700

    a

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,15 +2,14 @@
 
 /**
  * main - check the code
- *
+ * print_alphabet - prints alphabet in lowercase
+ * Description: function print alphabet
  * Return: Always 0
  */
 void print_alphabet(void)
 {
-    int alpha = 'a';
-    for (alpha = 'a'; alpha <= 'z'; alpha++)
-    _putchar(alpha);
-    _putchar('\n');
-    
-    return (0);
+for (alpha = 'a'; alpha <= 'z'; alpha++)
+_putchar(alpha);
+_putchar('\n');
+return (0);
 }
