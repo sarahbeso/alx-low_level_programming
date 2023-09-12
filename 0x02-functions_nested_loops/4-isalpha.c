2eedcbd5268b868794a8f2f4cf9ff25a765d1fa5
sarah <messy.earth@gmail.com>
2023-09-12 14:07:41 -0700

    a

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *_isalpha - print 1 if lower and prints 0 if else
+ *@c : checks input of function
+ * Return: Always 0.
+ */
+int _isalpha(int c)
+{
+if (c >= 97 && c <= 122)
+if (c >= 65 && c <= 90)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
