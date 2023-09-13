3109eda5d782e4ed68d3b64df6cd1abb8e43572c
sarah <messy.earth@gmail.com>
2023-09-12 17:05:20 -0700

    a

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* add - add two inteder
+*@a: first input
+*@b: second input
+*Return: sum of a+b
+ */
+int add(int a, int b)
+{
+int sum;
+sum = a + b;
+return (sum);
+}
---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * jack_bauer - print the last digit
+ *@n: takes number input
+ * Return: Always 0.
+ */
+void jack_bauer(void);
+{
+
+}
