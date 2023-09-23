57850177ee76320307538bb451039b0e0f4892fb
sarah <messy.earth@gmail.com>
2023-09-23 06:20:12 -0700

    a

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -5,14 +5,14 @@
  * @dest: pointer to destnation input
  * @src: pponter to source input
  * Return: pointer to resulting string @dest
- * */
+ */
 char *_strcat(char *dest, char *src)
 {
 	int c, c2;
-	c=0;
+c = 0;
 	while (dest[c])
 		c++;
-	for (c2=0; src[c2] ; c2++)
+	for (c2 = 0; src[c2] ; c2++)
 		dest[c++] = src[c2];
 	return (dest);
 }
